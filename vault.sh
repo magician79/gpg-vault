77a12e280d0e21604e443d44e10a55e4cf89e9ac
Souvik Mitra <souvik.m@outlook.com>
2025-08-24 00:08:36 +0530

    Improve reliability of zip-before-encrypt with error checks and failure handling (v0.4.0)
    - Added error detection for zip compression and GPG encryption commands.
    - Ensured removal of originals only after successful encryption.
    - Improved user feedback on failures.
    - Refined option parsing and command structure for clarity.

--- vault.sh
+++ vault.sh
@@ -3,41 +3,37 @@
 ################################################################################
 # Decrypt                                                                      #
 ################################################################################
-Decrypt()
-{
-   # Decrypt folder or file
-	 if [[ "$1" == *.tar.gpg ]]; then
-	  echo "Decrypting folder '${1}' using gpgtar..."
-	  gpgtar --decrypt --directory . "$1"
-	  echo "Folder decrypted from '${1}' to '${1%.tar.gpg}'."
-	 elif [[ "$1" == *.gpg ]]; then
-	  echo "Decrypting file '${1}' using gpg..."
-	  gpg --decrypt "$1" > "${1%.gpg}"
-	  echo "File decrypted from '${1}' to '${1%.gpg}'."
-	 else
-	  echo "Error: '$1' does not appear to be an encrypted file or folder."
-	  exit 1
-	 fi
+Decrypt() {
+    if [[ "$1" == *.tar.gpg ]]; then
+        echo "Decrypting folder '$1' using gpgtar..."
+        gpgtar --decrypt --directory . "$1"
+        echo "Folder decrypted from '$1' to '${1%.tar.gpg}'."
+    elif [[ "$1" == *.gpg ]]; then
+        echo "Decrypting file '$1' using gpg..."
+        gpg --decrypt "$1" > "${1%.gpg}"
+        echo "File decrypted from '$1' to '${1%.gpg}'."
+    else
+        echo "Error: '$1' does not appear to be an encrypted file or folder."
+        exit 1
+    fi
 }
 
 ################################################################################
 # Encrypt                                                                      #
 ################################################################################
-Encrypt()
-{
-	# Encrypt folder or file
-	 if [ -d "$1" ]; then
-	  echo "Encrypting folder '$1' using gpgtar..."
-	  gpgtar --symmetric -o "${1}.tar.gpg" "$1" && rm -rf "$1"
-	  echo "Folder '$1' encrypted to '${1}.tar.gpg'."
-	 elif [ -f "$1" ]; then
-	  echo "Encrypting file '$1' using gpg..."
-	  gpg --symmetric -o "${1}.gpg" "$1" && rm -rf "$1"
-	  echo "File '$1' encrypted to '${1}.gpg'."
-	 else
-	  echo "Error: '$1' is neither a file nor a directory."
-	  exit 1
-	 fi
+Encrypt() {
+    if [ -d "$1" ]; then
+        echo "Encrypting folder '$1' using gpgtar..."
+        gpgtar --symmetric -o "${1}.tar.gpg" "$1" && rm -rf "$1"
+        echo "Folder '$1' encrypted to '${1}.tar.gpg'."
+    elif [ -f "$1" ]; then
+        echo "Encrypting file '$1' using gpg..."
+        gpg --symmetric -o "${1}.gpg" "$1" && rm -rf "$1"
+        echo "File '$1' encrypted to '${1}.gpg'."
+    else
+        echo "Error: '$1' is neither a file nor a directory."
+        exit 1
+    fi
 }
 
 ################################################################################
@@ -46,6 +42,7 @@ Encrypt()
 EncryptZip() {
     local TARGET="$1"
     local ZIPNAME="${TARGET}.zip"
+
     if [ -e "$ZIPNAME" ]; then
         echo "Warning: '$ZIPNAME' already exists."
         echo "Press Enter to [O]verwrite, S to [S]kip, R to [R]ename:"
@@ -57,44 +54,58 @@ EncryptZip() {
             *) echo "Invalid choice. Exiting."; exit 1;;
         esac
     fi
+
     echo "Zipping '$TARGET' to '$ZIPNAME'..."
     zip -r "$ZIPNAME" "$TARGET"
+    if [ $? -ne 0 ]; then
+        echo "Zip operation failed."; exit 1
+    fi
+
     echo "Encrypting zipped file '$ZIPNAME' using gpg..."
-    gpg --symmetric -o "${ZIPNAME}.gpg" "$ZIPNAME" && rm -rf "$TARGET" "$ZIPNAME"
-    echo "File or folder '$TARGET' zipped and encrypted to '${ZIPNAME}.gpg'."
+    gpg --symmetric -o "${ZIPNAME}.gpg" "$ZIPNAME"
+    if [ $? -eq 0 ]; then
+        rm -rf "$TARGET" "$ZIPNAME"
+        echo "File or folder '$TARGET' zipped and encrypted to '${ZIPNAME}.gpg'."
+    else
+        echo "Encryption failed."; exit 1
+    fi
 }
 
 ################################################################################
 # Help                                                                         #
 ################################################################################
-Help()
-{
-   # Display Help
-   echo "This is a tool to encrypt/decrypt files with gpg"
-   echo
-   echo "Syntax: vault [-o <file>] | [-l <file|folder> [-z]] | [-h]"
-   echo "options:"
-   echo "o     Opens the safe i.e. decrypyts the file provide"
-   echo "l     Locks the safe i.e. encrypts the file provided"
-   echo "z		 Zip the file or folder before encryption (use with -l)"
-   echo "h     Print this Help."
-   echo
+Help() {
+    echo "This is a tool to encrypt/decrypt files with gpg"
+    echo
+    echo "Syntax: vault [-o <file>] | [-l <file|folder> [-z]] | [-h]"
+    echo "options:"
+    echo "o Opens the safe i.e. decrypts the file provided"
+    echo "l Locks the safe i.e. encrypts the file or folder provided"
+    echo "z Zip the file or folder before encryption (use with -l)"
+    echo "h Print this Help."
+    echo
 }
 
+################################################################################
+
+# Main script logic
+
 if [[ $# -eq 0 ]]; then
-    # display Help
     Help
-    exit;
+    exit
 fi
+
+ENCRYPT_MODE="normal"
 
 while getopts "o:l:zh" option; do
     case $option in
         o) # decrypt
-            FNAME=$OPTARG
-            Decrypt $FNAME
-            exit;;
+            FNAME="$OPTARG"
+            Decrypt "$FNAME"
+            exit
+            ;;
         l) # encrypt
-            FNAME=$OPTARG
+            FNAME="$OPTARG"
             ENCRYPT_MODE="normal"
             ;;
         z) # zip before encrypt (sets mode)
@@ -106,7 +117,7 @@ while getopts "o:l:zh" option; do
 done
 
 if [[ $ENCRYPT_MODE == "zip" ]]; then
-    EncryptZip $FNAME
+    EncryptZip "$FNAME"
 else
-    Encrypt $FNAME
+    Encrypt "$FNAME"
 fi
